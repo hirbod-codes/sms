c0f682dbbb8fb06746c102343356d502432d9e85
Hirbod Khatami <hirbod.khatami@gmail.com>
2021-12-14 05:51:51 -0800

    Update sms.php

--- src/sms.php
+++ src/sms.php
@@ -7,20 +7,21 @@ use Illuminate\Support\Facades\Http;
 class sms
 {
     protected string $to;
+    
     protected string $text;
 
     public function __construct()
     {
     }
 
-    public function to()
+    public function to(string $to): self
     {
         $this->to = $to;
 
         return $this;
     }
 
-    public function text()
+    public function text(string $text): self
     {
         $this->text = $text;
 
@@ -30,22 +31,22 @@ class sms
     public function send()
     {
         $headers =
-        [
-            "Accept" => "application/json, text-plain, */*",
-            'content-type' => 'application/json',
-            'postman-token' => '3e37c158-2c35-75aa-1ae7-f76d90ebbe8f',
-            'cache-control' => 'no-cache'
-        ];
+            [
+                "Accept" => "application/json, text-plain, */*",
+                'content-type' => 'application/json',
+                'postman-token' => '3e37c158-2c35-75aa-1ae7-f76d90ebbe8f',
+                'cache-control' => 'no-cache'
+            ];
 
         $data =
-        [
-            'username' => config("SMS_USERNAME"),
-            'password' => config("SMS_PASSWORD"),
-            'to' => $this->to,
-            'from' => config("SMS_from"),
-            'text' => $this->text,
-            'isflash' => 'false'
-        ];
+            [
+                'username' => config("SMS_USERNAME"),
+                'password' => config("SMS_PASSWORD"),
+                'to' => $this->to,
+                'from' => config("SMS_from"),
+                'text' => $this->text,
+                'isflash' => 'false'
+            ];
 
         $response = Http::withHeaders($headers)->post(config('SMS_HOST'), $data);
 
