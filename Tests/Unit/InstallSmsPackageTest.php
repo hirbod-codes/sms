a32fd60f00e0de6fceaacfe73620f5d84c4506fd
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-17 22:43:11 -0700

    Update InstallSmsPackageTest test class

--- Tests/Unit/InstallSmsPackageTest.php
+++ Tests/Unit/InstallSmsPackageTest.php
@@ -11,7 +11,6 @@ class InstallSmsPackageTest extends TestCase
 {
     public function test_the_install_command_copies_the_configuration()
     {
-        // make sure we're starting from a clean state
         if (File::exists(config_path('smspackage.php'))) {
             unlink(config_path('smspackage.php'));
         }
@@ -27,26 +26,19 @@ class InstallSmsPackageTest extends TestCase
     {
         // Given we have already have an existing config file
         File::put(config_path('smspackage.php'), 'test contents');
-
         $this->assertTrue(File::exists(config_path('smspackage.php')));
 
-        // When we run the install command
         $command = $this->artisan('smspackage:install');
 
-        // We expect a warning that our configuration file exists
         $command->expectsConfirmation(
             'Config file already exists. Do you want to overwrite it?',
             // When answered with "no"
             'no'
         );
 
-        // We should see a message that our file was not overwritten
         $command->expectsOutput('Existing configuration was not overwritten');
-
-        // Assert that the original contents of the config file remain
         $this->assertEquals('test contents', file_get_contents(config_path('smspackage.php')));
 
-        // Clean up
         unlink(config_path('smspackage.php'));
     }
 
@@ -56,10 +48,8 @@ class InstallSmsPackageTest extends TestCase
         File::put(config_path('smspackage.php'), 'test contents');
         $this->assertTrue(File::exists(config_path('smspackage.php')));
 
-        // When we run the install command
         $command = $this->artisan('smspackage:install');
 
-        // We expect a warning that our configuration file exists
         $command->expectsConfirmation(
             'Config file already exists. Do you want to overwrite it?',
             // When answered with "yes"
@@ -68,16 +58,13 @@ class InstallSmsPackageTest extends TestCase
         
         // execute the command to force override 
         $command->execute();
-
         $command->expectsOutput('Overwriting configuration file...');
 
-        // Assert that the original contents are overwritten
         $this->assertEquals(
             file_get_contents(__DIR__.'/../../config/config.php'),
             file_get_contents(config_path('smspackage.php'))
         );
 
-        // Clean up
         unlink(config_path('smspackage.php'));
     }
 }
